c8e37a5a2e6f0330cb9de9fc00c9815fe7653882
rustam <rustam-aliev@hotmail.com>
2020-12-03 11:06:34 +0300

    5th - ftps, telegraf, influxdb, grafana added

--- setup.sh
+++ setup.sh
@@ -24,3 +24,23 @@ minikube kubectl -- apply -f ./srcs/wordpress/wordpress.yaml
 #Starting phpmyadmin 
 docker build -t phpmyadmin_image ./srcs/phpmyadmin/
 minikube kubectl -- apply -f ./srcs/phpmyadmin/phpmyadmin.yaml
+
+#Starting ftps 
+docker build -t ftps_image ./srcs/ftps/
+minikube kubectl -- apply -f ./srcs/ftps/ftps.yaml
+
+#Starting ftps2
+#docker build -t ftps_image ./srcs/ftps2/
+#minikube kubectl -- apply -f ./srcs/ftps2/ftps.yaml
+
+#Starting influxdb 
+docker build -t influxdb_image ./srcs/influxdb/
+minikube kubectl -- apply -f ./srcs/influxdb/influxdb.yaml
+
+#Starting telegraf 
+docker build -t telegraf_image ./srcs/telegraf/
+minikube kubectl -- apply -f ./srcs/telegraf/telegraf.yaml
+
+#Starting grafana 
+docker build -t grafana_image ./srcs/grafana/
+minikube kubectl -- apply -f ./srcs/grafana/grafana.yaml
